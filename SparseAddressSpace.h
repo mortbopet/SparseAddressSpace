e0d9d4199260156671360a4278ca459266108e44
Morten Borup Petersen <morten_bp@live.dk>
2020-06-22 10:33:14 +0200

    Implement segment insertion

--- SparseAddressSpace.h
+++ SparseAddressSpace.h
@@ -1,28 +1,45 @@
-#include <vector>
-#include <unordered_set>
 #include <memory>
+#include <set>
+#include <vector>
 
 #include "external/intervaltree/IntervalTree.h"
 
 template <typename T_addr, typename T_v, size_t minSegSize = 4 /** Minimum segment size, in bytes */>
 struct SparseAddressSpace {
-    struct Segment {
-        T_addr start;
-        std::vector<uint8_t> data;
+    struct Segment;
+    using SegSPtr = std::shared_ptr<Segment>;
+    using SegWPtr = std::weak_ptr<Segment>;
+    using T_interval = Interval<size_t, SegSPtr>;
+    using Range = std::pair<size_t, size_t>;
 
+    struct Segment {
+        Segment() {}
+
+        T_addr start;
+        inline T_addr end() const { return start + data.size(); }
         bool contains(const Segment& other) const {
             return start <= other.start && (start + data.size()) >= (other.start + other.data.size());
         }
+        T_interval toInterval() { return T_interval(start, end(), toSharedPtr()); }
+        bool operator==(const Segment& other) const { return start == other.start && data == other.data; }
+        std::vector<uint8_t> data;
 
-        bool operator=(const Segment& other) const {
-            return start == other.start && data == other.data;
+        std::shared_ptr<Segment> toSharedPtr() {
+            if (!m_ptr.expired()) {
+                return std::shared_ptr(m_ptr);
+            } else {
+                // Local pointer is uninitialized...
+                auto shptr = std::shared_ptr<Segment>(this);
+                m_ptr = shptr;
+                return shptr;
+            }
         }
+
+        // All references to this Segment is managed by a shared pointer pointing to m_ptr. m_ptr is a weak pointer, to
+        // ensure that this object itself does not maintain a reference to itself.
+        std::weak_ptr<Segment> m_ptr;
     };
 
-    using SegmentPtr = std::shared_ptr<Segment>;
-    using SegmentWPtr = std::weak_ptr<Segment>;
-    using T_interval = Interval<size_t, SegmentPtr>;
-    using Range = std::pair<size_t, size_t>;
     SparseAddressSpace() {}
 
     SparseAddressSpace(const T_addr startAddr, uint8_t* data, size_t n) { addInitArray(startAddr, data, n); }
@@ -72,7 +89,8 @@ struct SparseAddressSpace {
 
     /**
      * @brief addInitializationMemory
-     * The specified program will be added as a memory segment which will be loaded into this memory once it is reset.
+     * The specified program will be added as a memory segment which will be loaded into this memory
+     * once it is reset.
      */
     void addInitArray(const T_addr start, uint8_t* data, size_t n) {
         auto seg = std::make_shared<Segment>();
@@ -98,52 +116,60 @@ struct SparseAddressSpace {
     /**
      * @brief insertSegment
      * Inserts memory segment @p segment at the specified starting address.
-     * If the segment overlaps any other memory segments, these will be coalesced, with overlapping memory values being
-     * taken from the newly inserted segment.
-     * We only check for overlaps at the start and stop address. Any segments contained within the newly inserted
-     * segment will be deleted.
+     * If the segment overlaps any other memory segments, these will be coalesced, with overlapping
+     * memory values being taken from the newly inserted segment. We only check for overlaps at the
+     * start and stop address. Any segments contained within the newly inserted segment will be
+     * deleted.
      */
-    void insertSegment(SegmentPtr& segment) {
-        /** Struct wrapper around an interval pointer to ensure that std::set does not try to overload resolve with an iterator */
-        struct IntervalPtr {
-            T_interval* ptr;
-        };
-        std::unordered_set<IntervalPtr> segmentsToKeep;
-        data.visit_all([&](const auto& interval) { segmentsToKeep.insert(&interval); });
+    void insertSegment(SegSPtr& segment) {
+        /** Struct wrapper around an interval pointer to ensure that std::set does not try to
+         * overload resolve with an iterator*/
+        std::set<SegSPtr> segmentsToKeep;
+        data.visit_all([&](auto& interval) { segmentsToKeep.insert(interval.value); });
 
-        // Locate segments which are fully contained within the new segment. These contained segments shall be removed.
-        std::vector<T_interval> contained = data.findContained(segment->start, segment->start + segment->data.size());
+        // Locate segments which are fully contained within the new segment. These contained
+        // segments shall be removed.
+        std::vector<T_interval> contained = data.findContained(segment->start, segment->end());
         for (auto& i : contained) {
-            segmentsToKeep.erase({&i});
+            segmentsToKeep.erase(i.value);
         }
 
-        // Coalesce any overlapping upper and lower segments
-        const std::vector<T_addr> edges = {segment->start, static_cast<T_addr>(segment->start + segment->data.size())};
+        // Coalesce any overlapping upper and lower segments into the new segment
+        const std::vector<T_addr> edges = {segment->start, static_cast<T_addr>(segment->end())};
         for (T_addr edgeAddress : edges) {
-            std::vector<T_interval> overlaps = data.findContained(edgeAddress, edgeAddress);
+            std::vector<T_interval> overlaps = data.findOverlapping(edgeAddress, edgeAddress);
             for (auto& i : overlaps) {
-                segmentsToKeep.erase({&i});
+                segmentsToKeep.erase(i.value);
                 coalesce(i.value, segment);
             }
         }
 
-        std::vector<T_interval> segmentsToKeepVec(segmentsToKeep.begin(), segmentsToKeep.end());
+        // convert segments to keep into format required by IntervalTree
+        std::vector<T_interval> segmentsToKeepVec;
+        for (const auto& seg : segmentsToKeep) {
+            segmentsToKeepVec.push_back(seg->toInterval());
+        }
 
-        // Rebuild the interval tree with the new set of (coalesced) intervals
-        data = IntervalTree<size_t, SegmentPtr>(segmentsToKeepVec);
+        // Insert (coalesced) new segment into segments to keep
+        segmentsToKeepVec.push_back({segment->start, segment->end(), segment});
+
+        // Rebuild the interval tree with the new set of (coalesced) intervals. std::move is used due to the r-value
+        // reference constraint of the IntervalTree constructor
+        data = IntervalTree<size_t, SegSPtr>(std::move(segmentsToKeepVec));
     }
 
-    std::vector<SegmentWPtr> segments() const {
-        std::vector<SegmentWPtr> segs;
+    std::vector<SegWPtr> segments() const {
+        std::vector<SegWPtr> segs;
         data.visit_all([&](const auto& interval) { segs.emplace_back(interval.value); });
         return segs;
     }
 
     /**
      * @brief coalesce
-     * Coalesce two segments, using values of @p s2 for any overlapping addresses between @p s1 and @p s2.
+     * Coalesce two segments, using values of @p s2 for any overlapping addresses between @p s1 and
+     * @p s2.
      */
-    SegmentPtr coalesce(SegmentPtr s1, SegmentPtr s2) {
+    SegSPtr coalesce(SegSPtr s1, SegSPtr s2) {
         if (s2->contains(*s1)) {
             return s2;
         }
@@ -154,6 +180,7 @@ struct SparseAddressSpace {
         // Coalesce lower
         if (coalesce_n_bytes > 0) {
             s2->data.insert(s2->data.begin(), s1->data.begin(), s1->data.begin() + std::abs(coalesce_n_bytes));
+            s2->start = s1->start;
         }
 
         // Coalesce upper
@@ -166,5 +193,5 @@ struct SparseAddressSpace {
 
     std::vector<SparseAddressSpace> initData;
 
-    IntervalTree<size_t, SegmentPtr> data;
+    IntervalTree<size_t, SegSPtr> data;
 };
